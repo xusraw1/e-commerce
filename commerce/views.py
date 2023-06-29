63aeae38253f7759b19b8f1dbe9a5e4c72295819
Xusrav <xusrav200329@gmail.com>
2023-06-29 07:51:18 +0500

    Create home page view and templates, adding bootstrap css,js.

--- commerce/views.py
+++ commerce/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import View
+from products.models import Product
 
-# Create your views here.
+
+class IndexView(View):
+
+    def get(self, request):
+        products = Product.objects.all()
+        return render(request, 'index.html', {'products': products})
+
+
