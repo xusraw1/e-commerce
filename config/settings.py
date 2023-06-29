b8528c55b70a288f484abc781595b7214742db70
Xusrav <xusrav200329@gmail.com>
2023-06-29 06:15:30 +0500

    Create customer user and set path in settings.py

--- config/settings.py
+++ config/settings.py
@@ -126,3 +126,4 @@ MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.CustomUser'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    phone = models.CharField(max_length=15)
+    username = models.CharField(max_length=50)
+    img = models.ImageField(upload_to='avatars/', default='avatars/default.png')
+
+    def __str__(self):
+        return f"Username: {self.username}"
