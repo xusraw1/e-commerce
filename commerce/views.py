dd18ec0119c621e700e74f7a1857ad1d6c1a40e2
Xusrav <xusrav200329@gmail.com>
2023-06-29 08:07:59 +0500

    Add cattegory page and view

--- commerce/views.py
+++ commerce/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
-from products.models import Product
+from products.models import Product, Category
+
+
+def get_category(request):
+    categories = Category.objects.all()
+    return {'categories': categories}
 
 
 class IndexView(View):
@@ -10,3 +15,8 @@ class IndexView(View):
         return render(request, 'index.html', {'products': products})
 
 
+class CategoryView(View):
+    def get(self, request, category_name):
+        category = get_object_or_404(Category, name=category_name)
+        products = Product.objects.filter(category=category)
+        return render(request, 'category.html', {'products': products, 'category': category})
