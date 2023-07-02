030e4ebb238dc88f041c15b4fa19b6c15f590560
Xusrav <xusrav200329@gmail.com>
2023-07-02 07:11:39 +0500

    Add comment logic for adding comments and deleting

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import new_product, detail_product, product_update, product_delete
+from .views import new_product, detail_product, product_update, product_delete, new_comment, delete_comment
 
 urlpatterns = [
     path('new_product/', new_product, name='new'),
     path('product/<int:product_id>/', detail_product, name='product'),
     path('product/<int:product_id>/update/', product_update, name='update'),
     path('product/<int:product_id>/delete/', product_delete, name='delete'),
+    path('product/<int:product_id>/coment/new/', new_comment, name='coment'),
+    path("<int:product_id>/comment/<int:comment_id>/delete", delete_comment, name='comment_delete'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import ProductForm, UpdateProductForm
-from .models import ProductImage, Product
+from .models import ProductImage, Product, Comment
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
 
 @login_required(login_url='login')
@@ -22,7 +23,7 @@ def new_product(request):
 
 
 def detail_product(request, product_id):
-    product = Product.objects.get(id=product_id)
+    product = get_object_or_404(Product, id=product_id)
     return render(request, "product_detail.html", {'product': product})
 
 
@@ -48,6 +49,7 @@ def product_update(request, product_id):
         return redirect('commerce:index')
 
 
+@login_required(login_url='login')
 def product_delete(request, product_id):
     product = get_object_or_404(Product, id=product_id)
     if request.user == product.author:
@@ -60,3 +62,28 @@ def product_delete(request, product_id):
     else:
         messages.error(request, "You can`t delete product, because you don`t author for this prodcut!")
         return redirect('commerce:index')
+
+
+@login_required(login_url='login')
+def new_comment(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    if request.method == 'POST':
+        Comment.objects.create(
+            author=request.user,
+            product=product,
+            text=request.POST['body']
+        )
+        messages.info(request, "Comment successfully sended!")
+        return redirect('product', product_id)
+    else:
+        return HttpResponse("add comment")
+
+
+@login_required(login_url='login')
+def delete_comment(request, product_id, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id)
+    if comment.author == request.user:
+        comment.delete()
+        messages.info(request, "Comment successfully deleted!")
+        return redirect('product', product_id)
+    return redirect('product', product_id)
